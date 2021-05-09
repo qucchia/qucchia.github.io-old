69eee0f26839a5b69c1d4188f0dc9b7bf7e7c2e6
qucchia <yijods@gmail.com>
2021-05-09 19:25:55 +0200

    Add load file to Scratch project tools

--- tools/scratch-project-tools.js
+++ tools/scratch-project-tools.js
@@ -1,3 +1,30 @@
+let fileName = "project.sb3";
+let projectJson = {};
+let projectSb3;
+
+function loadFile() {
+	document.getElementById("loading").className = "visible";
+
+	let reader = new FileReader();
+	reader.onload = function() {
+			JSZip.loadAsync(reader.result).then(sb3 => {
+				projectSb3 = sb3;
+				sb3.file("project.json").async("string").then(project => {
+					projectJson = JSON.parse(project);
+					console.log(projectJson);
+				})
+			});
+	}
+
+	const target = document.getElementById("file");
+	fileName = target.files[0].name;
+	reader.readAsArrayBuffer(target.files[0]);
+
+	document.getElementById("loading").className = "hidden";
+	document.getElementById("tools").className = "visible";
+}
+
+
 function loadUrl() {
 	let url = document.getElementById("project-url").value;
 	let id;
@@ -14,13 +41,6 @@ function loadUrl() {
 	}
 	document.getElementById("loading").className = "visible";
 	// TODO: load project file
-	document.getElementById("loading").className = "hidden";
-	document.getElementById("tools").className = "visible";
-}
-
-function loadFromFile() {
-	document.getElementById("loading").className = "visible";
-	// TODO: load file
 	document.getElementById("loading").className = "hidden";
 	document.getElementById("tools").className = "visible";
 }
@@ -43,4 +63,21 @@ function addAction() {
 	document.getElementById("actions").appendChild(newAction);
 
 	document.getElementById("action").value = "";
+}
+
+function makeEditsAndDownload() {
+	// TODO: Make edits
+	projectJson.meta.semver = "3.0.1";
+
+	projectSb3.file("project.json", JSON.stringify(projectJson));
+
+	projectSb3.generateAsync({type:"base64"}).then(data => {
+		let link = document.createElement("a");
+    link.style.display = "none";
+    link.download = fileName;
+    link.href = "data:application/zip;base64," + data;
+    document.body.appendChild(link);
+    link.click();
+	});
+
 }
